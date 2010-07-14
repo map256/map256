340fb870d443818d297f18fe6a87bf976d171d4c
esigler <esigler@gmail.com>
2010-07-13 20:55:25 -0500

    Updating licensing on a file

--- m256.py
+++ m256.py
@@ -1,5 +1,27 @@
-from m256_cfg import *
-from models import *
+#!/usr/bin/env python
+
+#
+# Copyright (c) 2010 Eric Sigler, esigler@gmail.com
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+
 import os
 import sys
 
@@ -15,6 +37,9 @@ from google.appengine.ext.webapp import template
 from google.appengine.api import users
 from google.appengine.api import mail
 from google.appengine.api import memcache
+
+from m256_cfg import *
+from models import *
 
 foursquare_request_token_url = 'http://foursquare.com/oauth/request_token'
 foursquare_access_token_url = 'http://foursquare.com/oauth/access_token'
