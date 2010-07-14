cd21bb51dd18b72f2dcedf0572c0d88807adedbe
esigler <esigler@gmail.com>
2010-07-13 21:35:46 -0500

    A little UI cleanup, added auto-center w/default when viewing a profile

--- static/map256.js
+++ static/map256.js
@@ -4,6 +4,7 @@ var infowindows = new Array();
 var markers_visible = false;
 var gradient_visible = false;
 var marker_position = 0;
+var centerpt = new google.maps.LatLng(37.958135,-91.773429);
 
 function hex (c) {
 	var s = "0123456789abcdef";
@@ -108,6 +109,11 @@ function goto_earliest() {
 }
 
 function initialize() {
+	if (checkin_data.length > 0) {
+		var tmp = checkin_data[0]['location'].split(",");
+		centerpt = new google.maps.LatLng(tmp[0], tmp[1])
+	}
+
 	var mapOptions = {
 		zoom: 12,
 		center: centerpt,
@@ -118,11 +124,11 @@ function initialize() {
 
 	for (var i=1; i<checkin_data.length; i++) {
 		if (checkin_data[i-1]['location'] != checkin_data[i]['location']) {
-			var foo_a = checkin_data[i-1]['location'].split(",");
-			var foo_b = checkin_data[i]['location'].split(",");
+			var tmp_a = checkin_data[i-1]['location'].split(",");
+			var tmp_b = checkin_data[i]['location'].split(",");
 
 			lines.push(new google.maps.Polyline({
-				path: new Array(new google.maps.LatLng(foo_a[0], foo_a[1]), new google.maps.LatLng(foo_b[0], foo_b[1])),
+				path: new Array(new google.maps.LatLng(tmp_a[0], tmp_a[1]), new google.maps.LatLng(tmp_b[0], tmp_b[1])),
 				strokeColor: "#000000",
 				strokeOpacity: 1.0,
 				strokeWeight: 2
@@ -131,9 +137,9 @@ function initialize() {
 	}
 
 	for (var y in checkin_data) {
-		var foo_a = checkin_data[y]['location'].split(",");
+		var tmp = checkin_data[y]['location'].split(",");
 		markers.push(new google.maps.Marker({
-			position: new google.maps.LatLng(foo_a[0], foo_a[1]),
+			position: new google.maps.LatLng(tmp[0], tmp[1]),
 			map: map,
 			visible: false
 		}));
@@ -169,7 +175,7 @@ function createMarker(marker, infowindow, map) {
 function initialize_front() {
 	var mapOptions = {
 		zoom: 4,
-		center: new google.maps.LatLng(37.9523113,-91.7715052),
+		center: centerpt,
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
 
@@ -201,7 +207,7 @@ function initialize_front() {
 		});
 		
 		infowindows[key] = new google.maps.InfoWindow({
-			content: "<h1>"+key+"</h1>"+"<a href=\"/t/"+key+"\">See the map for this person</a>",
+			content: "<h4><a href=\"/t/"+key+"\">"+key+"</a></h4>",
 			maxWidth: 200
 		});
 	}
@@ -210,3 +216,311 @@ function initialize_front() {
 		createMarker(markers[key2], infowindows[key2], map);
 	}
 }
+
+/*
+    http://www.JSON.org/json_parse.js
+    2009-05-31
+
+    Public Domain.
+
+    NO WARRANTY EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.
+
+*/
+
+/*members "", "\"", "\/", "\\", at, b, call, charAt, f, fromCharCode,
+    hasOwnProperty, message, n, name, push, r, t, text
+*/
+
+var json_parse = (function () {
+
+// This is a function that can parse a JSON text, producing a JavaScript
+// data structure. It is a simple, recursive descent parser. It does not use
+// eval or regular expressions, so it can be used as a model for implementing
+// a JSON parser in other languages.
+
+// We are defining the function inside of another function to avoid creating
+// global variables.
+
+    var at,     // The index of the current character
+        ch,     // The current character
+        escapee = {
+            '"':  '"',
+            '\\': '\\',
+            '/':  '/',
+            b:    '\b',
+            f:    '\f',
+            n:    '\n',
+            r:    '\r',
+            t:    '\t'
+        },
+        text,
+
+        error = function (m) {
+
+// Call error when something is wrong.
+
+            throw {
+                name:    'SyntaxError',
+                message: m,
+                at:      at,
+                text:    text
+            };
+        },
+
+        next = function (c) {
+
+// If a c parameter is provided, verify that it matches the current character.
+
+            if (c && c !== ch) {
+                error("Expected '" + c + "' instead of '" + ch + "'");
+            }
+
+// Get the next character. When there are no more characters,
+// return the empty string.
+
+            ch = text.charAt(at);
+            at += 1;
+            return ch;
+        },
+
+        number = function () {
+
+// Parse a number value.
+
+            var number,
+                string = '';
+
+            if (ch === '-') {
+                string = '-';
+                next('-');
+            }
+            while (ch >= '0' && ch <= '9') {
+                string += ch;
+                next();
+            }
+            if (ch === '.') {
+                string += '.';
+                while (next() && ch >= '0' && ch <= '9') {
+                    string += ch;
+                }
+            }
+            if (ch === 'e' || ch === 'E') {
+                string += ch;
+                next();
+                if (ch === '-' || ch === '+') {
+                    string += ch;
+                    next();
+                }
+                while (ch >= '0' && ch <= '9') {
+                    string += ch;
+                    next();
+                }
+            }
+            number = +string;
+            if (isNaN(number)) {
+                error("Bad number");
+            } else {
+                return number;
+            }
+        },
+
+        string = function () {
+
+// Parse a string value.
+
+            var hex,
+                i,
+                string = '',
+                uffff;
+
+// When parsing for string values, we must look for " and \ characters.
+
+            if (ch === '"') {
+                while (next()) {
+                    if (ch === '"') {
+                        next();
+                        return string;
+                    } else if (ch === '\\') {
+                        next();
+                        if (ch === 'u') {
+                            uffff = 0;
+                            for (i = 0; i < 4; i += 1) {
+                                hex = parseInt(next(), 16);
+                                if (!isFinite(hex)) {
+                                    break;
+                                }
+                                uffff = uffff * 16 + hex;
+                            }
+                            string += String.fromCharCode(uffff);
+                        } else if (typeof escapee[ch] === 'string') {
+                            string += escapee[ch];
+                        } else {
+                            break;
+                        }
+                    } else {
+                        string += ch;
+                    }
+                }
+            }
+            error("Bad string");
+        },
+
+        white = function () {
+
+// Skip whitespace.
+
+            while (ch && ch <= ' ') {
+                next();
+            }
+        },
+
+        word = function () {
+
+// true, false, or null.
+
+            switch (ch) {
+            case 't':
+                next('t');
+                next('r');
+                next('u');
+                next('e');
+                return true;
+            case 'f':
+                next('f');
+                next('a');
+                next('l');
+                next('s');
+                next('e');
+                return false;
+            case 'n':
+                next('n');
+                next('u');
+                next('l');
+                next('l');
+                return null;
+            }
+            error("Unexpected '" + ch + "'");
+        },
+
+        value,  // Place holder for the value function.
+
+        array = function () {
+
+// Parse an array value.
+
+            var array = [];
+
+            if (ch === '[') {
+                next('[');
+                white();
+                if (ch === ']') {
+                    next(']');
+                    return array;   // empty array
+                }
+                while (ch) {
+                    array.push(value());
+                    white();
+                    if (ch === ']') {
+                        next(']');
+                        return array;
+                    }
+                    next(',');
+                    white();
+                }
+            }
+            error("Bad array");
+        },
+
+        object = function () {
+
+// Parse an object value.
+
+            var key,
+                object = {};
+
+            if (ch === '{') {
+                next('{');
+                white();
+                if (ch === '}') {
+                    next('}');
+                    return object;   // empty object
+                }
+                while (ch) {
+                    key = string();
+                    white();
+                    next(':');
+                    if (Object.hasOwnProperty.call(object, key)) {
+                        error('Duplicate key "' + key + '"');
+                    }
+                    object[key] = value();
+                    white();
+                    if (ch === '}') {
+                        next('}');
+                        return object;
+                    }
+                    next(',');
+                    white();
+                }
+            }
+            error("Bad object");
+        };
+
+    value = function () {
+
+// Parse a JSON value. It could be an object, an array, a string, a number,
+// or a word.
+
+        white();
+        switch (ch) {
+        case '{':
+            return object();
+        case '[':
+            return array();
+        case '"':
+            return string();
+        case '-':
+            return number();
+        default:
+            return ch >= '0' && ch <= '9' ? number() : word();
+        }
+    };
+
+// Return the json_parse function. It will have access to all of the above
+// functions and variables.
+
+    return function (source, reviver) {
+        var result;
+
+        text = source;
+        at = 0;
+        ch = ' ';
+        result = value();
+        white();
+        if (ch) {
+            error("Syntax error");
+        }
+
+// If there is a reviver function, we recursively walk the new structure,
+// passing each name/value pair to the reviver function for possible
+// transformation, starting with a temporary root object that holds the result
+// in an empty key. If there is not a reviver function, we simply return the
+// result.
+
+        return typeof reviver === 'function' ? (function walk(holder, key) {
+            var k, v, value = holder[key];
+            if (value && typeof value === 'object') {
+                for (k in value) {
+                    if (Object.hasOwnProperty.call(value, k)) {
+                        v = walk(value, k);
+                        if (v !== undefined) {
+                            value[k] = v;
+                        } else {
+                            delete value[k];
+                        }
+                    }
+                }
+            }
+            return reviver.call(holder, key, value);
+        }({'': result}, '')) : result;
+    };
+}());
