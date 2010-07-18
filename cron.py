a98ccd64ed70c64d518a9e93e085f266ab947dbf
esigler <esigler@gmail.com>
2010-07-18 14:07:20 -0500

    Looking at a few Python cleanup tasks

--- cron.py
+++ cron.py
@@ -33,71 +33,71 @@ from google.appengine.api.labs import taskqueue
 from models import *
 
 class FoursquareHistoryDispatcher(webapp.RequestHandler):
-	def get(self):
-		q1 = FoursquareAccount.all()
-		q1.filter('foursquare_disabled = ', False)
+    def get(self):
+        q1 = FoursquareAccount.all()
+        q1.filter('foursquare_disabled = ', False)
 
-		for user in q1:
-			params = {}
-			params['fsq_id'] = user.foursquare_id
+        for user in q1:
+            params = {}
+            params['fsq_id'] = user.foursquare_id
 
-			q2 = FoursquareCheckin.all()
-			q2.filter('owner = ', user)
-			q2.order('-checkin_id')
+            q2 = FoursquareCheckin.all()
+            q2.filter('owner = ', user)
+            q2.order('-checkin_id')
 
-			if q2.count() > 0:
-				latest = q2.get()
-				params['since'] = latest.checkin_id
+            if q2.count() > 0:
+                latest = q2.get()
+                params['since'] = latest.checkin_id
 
-			logging.info('Enqueing task worker_foursquare_history with params %s' % params)
-			taskqueue.add(url='/worker_foursquare_history', params=params, method='GET')
+            logging.info('Enqueing task worker_foursquare_history with params %s' % params)
+            taskqueue.add(url='/worker_foursquare_history', params=params, method='GET')
 
 class TwitterHistoryDispatcher(webapp.RequestHandler):
-	def get(self):
-		q1 = TwitterAccount.all()
-		q1.filter('disabled =', False)
+    def get(self):
+        q1 = TwitterAccount.all()
+        q1.filter('disabled =', False)
 
-		for user in q1:
-			params = {}
-			params['twitter_id'] = user.twitter_id
+        for user in q1:
+            params = {}
+            params['twitter_id'] = user.twitter_id
 
-			q2 = TwitterCheckin.all()
-			q2.filter('owner =', user)
-			q2.order('-tweet_id')
+            q2 = TwitterCheckin.all()
+            q2.filter('owner =', user)
+            q2.order('-tweet_id')
 
-			if q2.count() > 0:
-				latest = q2.get()
-				params['since'] = latest.tweet_id
-			elif user.most_recent_tweet_id is not None:
-				params['since'] = user.most_recent_tweet_id
+            if q2.count() > 0:
+                latest = q2.get()
+                params['since'] = latest.tweet_id
+            elif user.most_recent_tweet_id is not None:
+                params['since'] = user.most_recent_tweet_id
 
-			logging.info('Enqueing task worker_twitter_history with params %s' % params)
-			taskqueue.add(url='/worker_twitter_history', params=params, method='GET')
+            logging.info('Enqueing task worker_twitter_history with params %s' % params)
+            taskqueue.add(url='/worker_twitter_history', params=params, method='GET')
 
 class StatisticsDispatcher(webapp.RequestHandler):
-	def get(self):
-		periods = ('week', 'month', 'alltime')
-		kinds = ('checkin_speed', 'distance_traveled', 'number_checkins')
+    def get(self):
+        periods = ('week', 'month', 'alltime')
+        kinds = ('checkin_speed', 'distance_traveled', 'number_checkins')
 
-		for kind in kinds:
-			for period in periods:
-				taskqueue.add(url='/worker_statistics', params={'period': period, 'kind': kind}, method='GET')
+        for kind in kinds:
+            for period in periods:
+                taskqueue.add(url='/worker_statistics', params={'period': period, 'kind': kind}, method='GET')
 
 class OauthRequestCleanupDispatcher(webapp.RequestHandler):
-	def get(self):
-		oldest_allowed = datetime.datetime.now() - datetime.timedelta(hours=8)
-		q1 = OauthRequest.all()
-		q1.filter('created <=', oldest_allowed)
-		db.delete(q1.fetch(250))
+    def get(self):
+        oldest_allowed = datetime.datetime.now() - datetime.timedelta(hours=8)
+        q1 = OauthRequest.all()
+        q1.filter('created <=', oldest_allowed)
+        db.delete(q1.fetch(250))
 
 def main():
-	application = webapp.WSGIApplication([('/cron_foursquare_history', FoursquareHistoryDispatcher),
-										  ('/cron_twitter_history', TwitterHistoryDispatcher),
-										  ('/cron_oauth_request_cleanup', OauthRequestCleanupDispatcher),
-										  ('/cron_statistics', StatisticsDispatcher)],
-		                                  debug=True)
+    application = webapp.WSGIApplication([('/cron_foursquare_history', FoursquareHistoryDispatcher),
+                                          ('/cron_twitter_history', TwitterHistoryDispatcher),
+                                          ('/cron_oauth_request_cleanup', OauthRequestCleanupDispatcher),
+                                          ('/cron_statistics', StatisticsDispatcher)],
+                                          debug=True)
 
-	util.run_wsgi_app(application)
+    util.run_wsgi_app(application)
 
 if __name__ == '__main__':
-	main()
+    main()
